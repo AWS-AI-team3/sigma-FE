5ca2ea8bcbca3e92ac16ffda9c261fab06240c4f
ashcircle03 <ashcircle03@gmail.com>
2025-10-09 16:12:48 +0900

    Initial Commit

---
+++ sigma/sigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sigmaApp.swift
+//  sigma
+//
+//  Created by ashcircle on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct sigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
